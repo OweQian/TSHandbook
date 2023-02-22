9ee4ef3aee972597ce9d8abb1e92ab62ebcf070c
qian wang <1510106069@qq.com>
2023-02-22 11:15:43 +0800

    feat: 原始类型的类型注解

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log('hello typescript')
+const name: string = 'wangxiaobia';
+const age: number = 18;
+const male: boolean = false;
+const undef: undefined = undefined;
+const nul: null = null;
+const bigIntVar1: bigint = 9007199254740991n;
+const symbolVar: symbol = Symbol('unique');
+
+const temp1: null = null;
+const temp2: undefined = undefined;
+const temp3: string = null;
+const temp4: string = undefined;
+
+function func1 () {};
+function func2 () { return };
+function func3 () {
+  return undefined;
+}
+
+const voidVar1: void = null;
+const voidVar2: void = undefined;
