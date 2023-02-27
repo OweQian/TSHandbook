3787c670ad9aed68534f7e37a11f26683905c1b8
qian wang <1510106069@qq.com>
2023-02-27 16:13:58 +0800

    feat: 函数类型

--- index.ts
+++ index.ts
@@ -1,32 +1,32 @@
-const name: string = 'wangxiaobia';
-const age: number = 18;
-const male: boolean = false;
-const undef: undefined = undefined;
-const nul: null = null;
-const bigIntVar1: bigint = 9007199254740991n;
-const symbolVar: symbol = Symbol('unique');
-
-const temp1: null = null;
-const temp2: undefined = undefined;
-const temp3: string = null;
-const temp4: string = undefined;
-
-function func1 () {};
-function func2 () { return };
-function func3 () {
-  return undefined;
-}
-
-const voidVar1: void = null;
-const voidVar2: void = undefined;
-
-const arr1: string[] = [];
-const arr2: Array<string> = [];
-
-const arr3: [string, string, string] = ['wang', 'xiao', 'bai'];
-arr3[599];
-
-const arr4: [string, number, boolean] = ['wang', 599, true];
+// const name: string = 'wangxiaobia';
+// const age: number = 18;
+// const male: boolean = false;
+// const undef: undefined = undefined;
+// const nul: null = null;
+// const bigIntVar1: bigint = 9007199254740991n;
+// const symbolVar: symbol = Symbol('unique');
+//
+// const temp1: null = null;
+// const temp2: undefined = undefined;
+// const temp3: string = null;
+// const temp4: string = undefined;
+//
+// function func1 () {};
+// function func2 () { return };
+// function func3 () {
+//   return undefined;
+// }
+//
+// const voidVar1: void = null;
+// const voidVar2: void = undefined;
+//
+// const arr1: string[] = [];
+// const arr2: Array<string> = [];
+//
+// const arr3: [string, string, string] = ['wang', 'xiao', 'bai'];
+// arr3[599];
+//
+// const arr4: [string, number, boolean] = ['wang', 599, true];
 
 // const arr5: [string, number?, boolean?] = ['wang'];
 //
@@ -105,3 +105,73 @@ const arr4: [string, number, boolean] = ['wang', 599, true];
 // const temp32: {} = [];
 //
 // temp31.age = 18;
+
+// function foo (name: string): number {
+//   return name.length;
+// }
+//
+// const foo = function (name: string): number {
+//   return name.length;
+// }
+//
+// const foo = (name: string): number => {
+//   return name.length;
+// }
+//
+// const foo: (name: string) => number = (name) => {
+//   return name.length;
+// }
+//
+// type FuncFoo = (name: string) => number;
+//
+// const foo: FuncFoo = (name) => {
+//   return name.length;
+// }
+//
+// interface IFuncFoo {
+//   (name: string): number;
+// }
+//
+// function foo(): void {}
+//
+// function bar(): undefined { return }
+//
+// function foo1 (name: string, age?: number): number {
+//   const inputAge = age ?? 18;
+//   return name.length + inputAge;
+// }
+//
+// function foo2 (name: string, age: number = 18): number {
+//   const inputAge = age ?? 18;
+//   return name.length + inputAge;
+// }
+//
+// function foo (arg1: string, ...rest: any[]) {}
+//
+// function foo (arg1: string, ...rest: [number, boolean]) {}
+//
+// function func (foo: number, bar?: boolean): string | number {
+//   if (bar) {
+//     return String(foo);
+//   } else {
+//     return foo * 18;
+//   }
+// }
+//
+// function func (foo: number, bar: false): number;
+// function func (foo: number, bar?: true): string;
+// function func (foo: number, bar?: boolean): string | number {
+//   if (bar) {
+//     return String(foo);
+//   } else {
+//     return foo * 18;
+//   }
+// }
+//
+// const res1 = func(18); // number
+// const res2 = func(18, true); // string
+// const res3 = func(18, false); // number
+//
+// async function asyncFunc(): Promise<void> {}
+// function* genFunc(): Iterable<void> {}
+// async function* asyncGenFunc(): AsyncIterable<void> {}
